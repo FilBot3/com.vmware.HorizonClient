669d354456e677267180d70f99b6a825617b2b77
Doomsdayrs <doomsdayrs@gmail.com>
2022-02-02 17:13:50 -0500

    Fixed up some lib paths, issues with udev

--- build.sh
+++ build.sh
@@ -21,6 +21,13 @@ cp "/app/share/icons/vmware-view.png" "/app/share/icons/hicolor/82x82/com.vmware
 cp "/app/share/applications/vmware-view.desktop" "/app/share/applications/com.vmware.HorizonClient.desktop"
 
 sed -i 's/\/usr/\/app/' "/app/bin/vmware-view"
+sed -i 's/\/usr/\/app/' "/app/lib/vmware/view/env/env_utils.sh"
+sed -i 's/\/usr/\/app/' "/app/lib/vmware/view/dct/vmware-view-log-collector"
+
+echo "Linking"
+cd /app/lib/
+ln -s libudev.so.1 libudev.so.0
+cd
 
 echo "Clean up"
 rm -rf "VMware-Horizon-Client-Linux-${VERSION}"*
